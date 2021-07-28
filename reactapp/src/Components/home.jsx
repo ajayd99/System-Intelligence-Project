d22ac3dd94108b012a5d6befabc2f39e66fe9c44
ajayd99 <a.dhalliwal@mail.com>
2021-07-28 22:19:54 +0100

    Carousel not working
    initial fix for prior bug issues, but carousel remains not displaying

--- reactapp/src/Components/home.jsx
+++ reactapp/src/Components/home.jsx
@@ -1,291 +1,69 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+//import { carousel } from "../../bootstrap/js/bootstrap.bundle";
+import Carousel from 'react-bootstrap/Carousel';
+
 
 function Home(props) {
   return (
-    
     <div>
-    <section id="hero">
-      <div id="heroCarousel" data-bs-interval={5000} className="carousel slide carousel-fade" data-bs-ride="carousel">
-        <ol className="carousel-indicators" id="hero-carousel-indicators" />
-        <div className="carousel-inner" role="listbox">
-          {/* Slide 1 */}
-          <div className="carousel-item active" style={{}}>
-            <div className="carousel-container">
-              <div className="container">
-                <h2 className="animate__animated animate__fadeInDown">Welcome to <span>Sailor</span></h2>
-                <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="#about" className="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-              </div>
-            </div>
+      <Carousel>
+        <Carousel.Item>
+          <img classname="d-block w-100" src="slide-1.jpg" alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+        <Carousel.Item>
+          <img classname="d-block w-100" src="slide-2.jpg" alt="Second slide" />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        
+        <Carousel.Item>
+          <img classname="d-block w-100" src="slide-3.jpg" alt="Third slide" />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+
+  <main id="main">
+    {/* ======= About Section ======= */}
+    <section id="about" className="about">
+      <div className="container">
+        <div className="row content">
+          <div className="col-lg-6">
+            <h2>Eum ipsam laborum deleniti velitena</h2>
+            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
           </div>
-          {/* Slide 2 */}
-          <div className="carousel-item" style={{}}>
-            <div className="carousel-container">
-              <div className="container">
-                <h2 className="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-                <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="#about" className="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-              </div>
-            </div>
-          </div>
-          {/* Slide 3 */}
-          <div className="carousel-item" style={{}}>
-            <div className="carousel-container">
-              <div className="container">
-                <h2 className="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-                <p className="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-                <a href="#about" className="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-              </div>
-            </div>
+          <div className="col-lg-6 pt-4 pt-lg-0">
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+            <ul>
+              <li><i className="ri-check-double-line" /> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
+              <li><i className="ri-check-double-line" /> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+              <li><i className="ri-check-double-line" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+            </ul>
+            <p className="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
           </div>
         </div>
-        <a className="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true" />
-        </a>
-        <a className="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
-          <span className="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true" />
-        </a>
       </div>
-    </section>
-    <main id="main">
-      {/* ======= About Section ======= */}
-      <section id="about" className="about">
-        <div className="container">
-          <div className="row content">
-            <div className="col-lg-6">
-              <h2>Eum ipsam laborum deleniti velitena</h2>
-              <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
-            </div>
-            <div className="col-lg-6 pt-4 pt-lg-0">
-              <p>
-                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum
-              </p>
-              <ul>
-                <li><i className="ri-check-double-line" /> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-                <li><i className="ri-check-double-line" /> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-                <li><i className="ri-check-double-line" /> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
-              </ul>
-              <p className="fst-italic">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                magna aliqua.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>{/* End About Section */}
-      {/* ======= Clients Section ======= */}
-      <section id="clients" className="clients section-bg">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-1.png" className="img-fluid" alt />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-2.png" className="img-fluid" alt />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-3.png" className="img-fluid" alt />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-4.png" className="img-fluid" alt />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-5.png" className="img-fluid" alt />
-            </div>
-            <div className="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-              <img src="assets/img/clients/client-6.png" className="img-fluid" alt />
-            </div>
-          </div>
-        </div>
-      </section>{/* End Clients Section */}
-      {/* ======= Services Section ======= */}
-      <section id="services" className="services">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="icon-box">
-                <i className="bi bi-briefcase" />
-                <h4><a href="#">Lorem Ipsum</a></h4>
-                <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
-              </div>
-            </div>
-            <div className="col-md-6 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i className="bi bi-card-checklist" />
-                <h4><a href="#">Dolor Sitema</a></h4>
-                <p>Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-              </div>
-            </div>
-            <div className="col-md-6 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i className="bi bi-bar-chart" />
-                <h4><a href="#">Sed ut perspiciatis</a></h4>
-                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-              </div>
-            </div>
-            <div className="col-md-6 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i className="bi bi-binoculars" />
-                <h4><a href="#">Nemo Enim</a></h4>
-                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-              </div>
-            </div>
-            <div className="col-md-6 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i className="bi bi-brightness-high" />
-                <h4><a href="#">Magni Dolore</a></h4>
-                <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-              </div>
-            </div>
-            <div className="col-md-6 mt-4 mt-md-0">
-              <div className="icon-box">
-                <i className="bi bi-calendar4-week" />
-                <h4><a href="#">Eiusmod Tempor</a></h4>
-                <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>{/* End Services Section */}
-      {/* ======= Portfolio Section ======= */}
-      <section id="portfolio" className="portfolio">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12 d-flex justify-content-center">
-              <ul id="portfolio-flters">
-                <li data-filter="*" className="filter-active">All</li>
-                <li data-filter=".filter-app">App</li>
-                <li data-filter=".filter-card">Card</li>
-                <li data-filter=".filter-web">Web</li>
-              </ul>
-            </div>
-          </div>
-          <div className="row portfolio-container">
-            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-1.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>App 1</h4>
-                  <p>App</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 1"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-2.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Web 3</h4>
-                  <p>Web</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 3"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-3.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>App 2</h4>
-                  <p>App</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 2"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-4.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Card 2</h4>
-                  <p>Card</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Card 2"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-5.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Web 2</h4>
-                  <p>Web</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 2"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-6.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>App 3</h4>
-                  <p>App</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="App 3"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-7.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Card 1</h4>
-                  <p>Card</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Card 1"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-card">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-8.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Card 3</h4>
-                  <p>Card</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Card 3"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div className="portfolio-wrap">
-                <img src="assets/img/portfolio/portfolio-9.jpg" className="img-fluid" alt />
-                <div className="portfolio-info">
-                  <h4>Web 3</h4>
-                  <p>Web</p>
-                  <div className="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox" title="Web 3"><i className="bx bx-plus" /></a>
-                    <a href="portfolio-details.html" className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details"><i className="bx bx-link" /></a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>{/* End Portfolio Section */}
-    </main>
-  </div>
+    </section>{/* End About Section */}
+  </main>
+    </div>
 
     );
 }  
