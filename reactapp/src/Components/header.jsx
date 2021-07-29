588918aad92d01f0323cf366b591c8f94de7f64c
ajayd99 <a.dhalliwal@mail.com>
2021-07-29 15:40:07 +0100

    Changes to Header component
    attempted to change the navbar with correct links

--- reactapp/src/Components/header.jsx
+++ reactapp/src/Components/header.jsx
@@ -1,43 +1,42 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import logo from '../SIL-logo.PNG';
+import home from '../components/Home.jsx';
+import services from '../components/Services.jsx';
+import about from '../components/About.jsx';
+import contact from '../components/Contact.jsx';
+
+
+
 
 function Header(props) {
   return (
 
     <header id="header" className="fixed-top d-flex align-items-center">
-    <div className="container d-flex align-items-center">
-      <h1 className="logo me-auto"><a href="index.html">Sailor</a></h1>
-      <a href="index.html" className="logo me-auto"><img src="assets/img/logo.png" alt className="img-fluid" /></a>
-      <nav id="navbar" className="navbar">
-        <ul>
-          <li><a href="index.html" className="active">Home</a></li>
-          <li className="dropdown"><a href="#"><span>About</span> <i className="bi bi-chevron-down" /></a>
-            <ul>
-              <li><a href="about.html">About</a></li>
-              <li><a href="team.html">Team</a></li>
-              <li><a href="testimonials.html">Testimonials</a></li>
-              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right" /></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li><a href="services.html">Services</a></li>
-          <li><a href="portfolio.html">Portfolio</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li><a href="contact.html">Contact</a></li>
-          <li><a href="index.html" className="getstarted">Get Started</a></li>
-        </ul>
-        <i className="bi bi-list mobile-nav-toggle" />
-      </nav>
-    </div>
-  </header>
+      <div className="container d-flex align-items-center">
+        <h1 className="logo me-auto"><a href= {home} >System Intelligence Ltd.</a></h1>
+        <a href="index.html" className="logo me-auto"><img src= {logo} alt className="img-fluid" /></a>
+        
+        <nav id="navbar" className="navbar">
+          <ul>
+            <li><a href= {home} className="active">Home</a></li>
+            <li className="dropdown"><a href= {services}><span>Services</span> <i className="bi bi-chevron-down" /></a>
+              <ul>
+                <li><a href="about.html">Consultancy</a></li>
+                <li><a href="team.html">Development</a></li>
+                <li><a href="testimonials.html">Recruitment</a></li>
+                <li><a href="testimonials.html">System Integration</a></li>
+                <li><a href="testimonials.html">Cloud</a></li>
+              </ul>
+            </li>
+            <li><a href= {about}>About</a></li>
+            <li><a href= {contact} className="getstarted">Contact Us</a></li>
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle" />
+        </nav>
+        
+      </div>
+    </header>
 
     );
 }  
