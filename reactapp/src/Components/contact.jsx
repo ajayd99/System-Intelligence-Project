e687a2413aebc56cb64a1797b3ebdce69ab70eb1
ajayd99 <a.dhalliwal@mail.com>
2021-07-29 16:50:34 +0100

    Update to Contact component
    Removing map from contact component

--- reactapp/src/Components/contact.jsx
+++ reactapp/src/Components/contact.jsx
@@ -4,30 +4,6 @@ import { Link, withRouter } from "react-router-dom";
 function Contact(props) {
   return (
     <section id="contact" className="contact">
-  <div className="container">
-    <div>
-      <iframe style={{"border":"0","width":"100%","height":"270px"}} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameBorder={0} allowFullScreen />
-    </div>
-    <div className="row mt-5">
-      <div className="col-lg-4">
-        <div className="info">
-          <div className="address">
-            <i className="bi bi-geo-alt" />
-            <h4>Location:</h4>
-            <p>A108 Adam Street, New York, NY 535022</p>
-          </div>
-          <div className="email">
-            <i className="bi bi-envelope" />
-            <h4>Email:</h4>
-            <p>info@example.com</p>
-          </div>
-          <div className="phone">
-            <i className="bi bi-phone" />
-            <h4>Call:</h4>
-            <p>+1 5589 55488 55s</p>
-          </div>
-        </div>
-      </div>
       <div className="col-lg-8 mt-5 mt-lg-0">
         <form action="forms/contact.php" method="post" role="form" className="php-email-form">
           <div className="row">
@@ -52,9 +28,8 @@ function Contact(props) {
           <div className="text-center"><button type="submit">Send Message</button></div>
         </form>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
+
   );
 
 }
