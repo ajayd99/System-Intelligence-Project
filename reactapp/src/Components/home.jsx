99e9b5e8fae25abcd03f18b8bdeb994f6455ca58
ajayd99 <a.dhalliwal@mail.com>
2021-07-29 12:52:59 +0100

    Working image carousel
    Moved the slide images to within the src folder and then imported them into the file where needed. More items may need to be moved into the src to work.

--- reactapp/src/Components/home.jsx
+++ reactapp/src/Components/home.jsx
@@ -2,36 +2,63 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 //import { carousel } from "../../bootstrap/js/bootstrap.bundle";
 import Carousel from 'react-bootstrap/Carousel';
-
+//import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import image1 from '../slide/slide-1.jpg'
+import image2 from '../slide/slide-2.jpg'
+import image3 from '../slide/slide-3.jpg'
 
 function Home(props) {
   return (
     <div>
-      <Carousel>
+      <div className='container-fluid' >
+        <div className="row">
+        <div className="col-sm-12">
+        <h3>React Bootstrap Carousel</h3>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-12">
+        <Carousel>
         <Carousel.Item>
-          <img classname="d-block w-100" src="slide-1.jpg" alt="First slide" />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
+        <img
+        className="d-block w-100"
+        src= {image1}
+        alt="First slide"
+        />
+        <Carousel.Caption>
+        <h3>First slide label</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
         </Carousel.Item>
-        
+
         <Carousel.Item>
-          <img classname="d-block w-100" src="slide-2.jpg" alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
+        <img
+        className="d-block w-100"
+        src= {image2}
+        alt="Second slide"
+        />
+        <Carousel.Caption>
+        <h3>Second slide label</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
         </Carousel.Item>
-        
+
         <Carousel.Item>
-          <img classname="d-block w-100" src="slide-3.jpg" alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
+        <img
+        className="d-block w-100"
+        src= {image3}
+        alt="Third slide"
+        />
+        <Carousel.Caption>
+        <h3>Third slide label</h3>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
         </Carousel.Item>
-      </Carousel>
+        </Carousel>
+        </div>
+        </div>
+      </div>
 
 
   <main id="main">
