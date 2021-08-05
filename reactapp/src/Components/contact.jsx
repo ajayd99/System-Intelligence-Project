10024b142f07416e04c0639fb3e70a57f40581a3
lachmand <latch@sysint.biz>
2021-08-05 10:45:44 +0100

    Update contact.jsx

--- reactapp/src/Components/contact.jsx
+++ reactapp/src/Components/contact.jsx
@@ -1,7 +1,45 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
-function Contact(props) {
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email:'', phone:'', subject:'', message:''};
+  }
+
+  handleNameChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleEmailChange = (event) => {
+    this.setState({[event.target.email]: event.target.value});
+  }
+  handlePhoneChange = (event) => {
+    this.setState({[event.target.phone]: event.target.value});
+  }
+  handleSubjectChange = (event) => {
+    this.setState({[event.target.subject]: event.target.value});
+  }
+  handleMessageChange = (event) => {
+    this.setState({[event.target.message]: event.target.value});
+  }
+
+
+  handleSubmit = (event) => {
+    alert('A form was submitted: ' + this.state);
+
+    fetch('https://sil-web-cosmosdb-fa.azurewebsites.net/api/enquiry-post?', {
+        method: 'POST',
+        // We convert the React state to JSON and send it as the POST body
+        body: JSON.stringify(this.state)
+      }).then(function(response) {
+        console.log(response)
+        return response.json();
+      });
+
+    event.preventDefault();
+}
+
+render(){
   return (
 
     <div>
@@ -51,23 +89,22 @@ function Contact(props) {
             </div>
           </div>
         </div>
-   
 
       <div className="col-lg-8 mt-5 mt-lg-0">
-        <form action="forms/contact.php" method="post" role="form" className="php-email-form">
+        <form action={this.handleSubmit} method="post" role="form" className="php-email-form">
           <div className="row">
             <div className="col-md-6 form-group">
-              <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+              <input type="text" value={this.state.name.value} name="name" onChange={this.handleNameChange} className="form-control" id="name" placeholder="Your Name" required />
             </div>
             <div className="col-md-6 form-group mt-3 mt-md-0">
-              <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+              <input type="email" value={this.state.email.value} name="email" onChange={this.handleEmailChange} className="form-control" id="email" placeholder="Your Email" required />
             </div>
             <div className="col-md-6 form-group mt-3 mt-md-0">
-              <input type="number" className="form-control" name="phone" id="phone" placeholder="Your Telephone Number" required />
+              <input type="number" value={this.state.phone.value} name="phone" onChange={this.handlePhoneChange} className="form-control" id="phone" placeholder="Your Telephone Number" required />
             </div>
             <div className="col-md-6 form-group mt-3 mt-md-0">
-              <select className="form-control" name="subject" id="subject" required>
-                <option value="none" selected disabled hidden>
+              <select name="subject" onChange={this.handleSubjectChange} className="form-control" id="subject" required >
+                <option value={this.state.subject.value} selected disabled hidden>
                   Select a Subject
                </option>
                 <option value="volvo">Consultancy</option>
@@ -79,7 +116,7 @@ function Contact(props) {
             </div>
           </div>
           <div className="form-group mt-3">
-            <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
+            <textarea value={this.state.message.value} name="message" rows={5} placeholder="Message" onChange={this.handleMessageChange} className="form-control" id="message" defaultValue={""} required />
           </div>
           <div className="my-3">
             <div className="loading">Loading</div>
@@ -95,6 +132,7 @@ function Contact(props) {
     </div>
 
   );
+  }
 
 }
 
